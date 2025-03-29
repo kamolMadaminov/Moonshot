9aafcda9a14186f184fb3a5952eb6e5c128208b5
developerkamol <developerkamol@gmail.com>
2025-03-29 21:42:15 +0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kamol Madaminov on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
